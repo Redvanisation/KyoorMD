33bf055fd03766d358edc02c02405c74e6f0f976
Redvan <redvanisation@gmail.com>
2020-05-07 16:18:30 -0400

    Add page 2 of the screener

--- src/containers/ScreenerCard.jsx
+++ src/containers/ScreenerCard.jsx
@@ -3,13 +3,14 @@ import { Link } from 'react-router-dom';
 import useLocalStorage from '../hooks/useLocalStorage';
 import backArrowSvg from '../img/icon-backarrow.svg';
 import pageOneImage from '../img/img-kyoormessage-wave.svg';
+import pageTwoImage from '../img/img-kyoormessage-stop.svg';
 
 const ScreenerCard = () => {
 
-  const [cardPagesProgress, setCardPagesProgress] = useLocalStorage('Card Pages Progress', 1);
+  const [cardPagesProgress, setCardPagesProgress] = useLocalStorage('Card Pages Progress', 3);
 
-  const setCardPages = (something) => {
-    switch (something) {
+  const setCardPages = (pageNumber) => {
+    switch (pageNumber) {
       case 1:
         return (
           <div className="card-content">
@@ -28,12 +29,82 @@ const ScreenerCard = () => {
                 <h4 className="title is-6 is-bold screener-card__completion-div--title">
                   How long does it take to complete?
                 </h4>
-                <p className="screener-card__completion-div--time">3 Min</p>
+                <p className="screener-card__completion-div--time green-text">3 Min</p>
                 <p className="screener-card__completion-div--time-text">Avg. Time</p>
 
               </div>
             </div>
-            <button type="button" className="btn screener-card__btn">Get Started</button>
+          </div>
+        );
+
+      case 2:
+        return (
+          <div className="card-content">
+            <div className="card-image">
+              <figure className="image screener-card__figure">
+                <img src={pageTwoImage} alt="vector" />
+              </figure>
+            </div>
+
+            <h3 className="title is-3 is-bold screener-card__title">If This is An Emergency!</h3>
+
+            <div className="content screener-card__content">
+              <p className="screener-card__content--part">
+                Please stop if this an emergency,&nbsp;
+                <span className="pink-text">call 911/112</span>
+                &nbsp;if you are experiencing:
+              </p>
+              <div className="screener-card__completion-div">
+                <ul className="screener-card__completion-div--ul">
+                  <li className="screener-card__completion-div--li">Persistent and severe chest pain</li>
+                  <li className="screener-card__completion-div--li">Extremely difficult breathing</li>
+                  <li className="screener-card__completion-div--li">Losing consciousness</li>
+                  <li className="screener-card__completion-div--li">Disorientation and/or confusion</li>
+                </ul>
+                <p className="screener-card__completion-div--text">
+                  This screening can&apos;t diagnose on its own, health professionals need more information to make the right diagnosis.
+                  &nbsp;
+                  <Link to="/" className="green-text">Legal</Link>
+                  &nbsp;
+                  Your provided information is anonymous view
+                  &nbsp;
+                  <Link to="/" className="green-text">Privacy Policy.</Link>
+                </p>
+              </div>
+            </div>
+          </div>
+        );
+
+      case 3:
+        return (
+          <div className="card-content">
+
+            <h3 className="title is-3 is-bold screener-card__title">If This is An Emergency!</h3>
+
+            <div className="content screener-card__content">
+              <p className="screener-card__content--part">
+                Please stop if this an emergency,&nbsp;
+                <span className="pink-text">call 911/112</span>
+                &nbsp;if you are experiencing:
+              </p>
+              <div className="screener-card__completion-div">
+                <ul className="screener-card__completion-div--ul">
+                  <li className="screener-card__completion-div--li">Persistent and severe chest pain</li>
+                  <li className="screener-card__completion-div--li">Extremely difficult breathing</li>
+                  <li className="screener-card__completion-div--li">Losing consciousness</li>
+                  <li className="screener-card__completion-div--li">Disorientation and/or confusion</li>
+                </ul>
+                <p className="screener-card__completion-div--text">
+                  This screening can&apos;t diagnose on its own, health professionals need more information to make the right diagnosis.
+                  &nbsp;
+                  <Link to="/" className="green-text">Legal</Link>
+                  &nbsp;
+                  Your provided information is anonymous view
+                  &nbsp;
+                  <Link to="/" className="green-text">Privacy Policy.</Link>
+                </p>
+              </div>
+            </div>
           </div>
         );
 
@@ -54,8 +125,9 @@ const ScreenerCard = () => {
           <Link to="/" className="screener-card__nav-div--item">Cancel</Link>
         </div>
         {/* Start */}
-        {setCardPages(1)}
+        {setCardPages(cardPagesProgress)}
         {/* End */}
+        <button type="button" className="btn screener-card__btn">{cardPagesProgress === 1 ? 'Get Started' : 'Next Page'}</button>
       </div>
     </div>
   );
